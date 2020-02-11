dfe074fd7d9ae66657bc8b4fe06cb4c309a9f6b7
Andrey Dreichan <dra@unicsoft.com.ua>
2020-02-11 16:51:10 +0200

    Updated gitignore file and custom module

--- web/modules/custom/client_info/src/Controller/Showinfo.php
+++ web/modules/custom/client_info/src/Controller/Showinfo.php
@@ -3,30 +3,33 @@
 namespace Drupal\client_info\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Symfony\Component\HttpFoundation;
 
 /**
  * Provides route responses for the custom module.
  */
 class Showinfo extends ControllerBase {
 
-  public function client_info() {
-  	$host = \Drupal::request()->getHost();
-   	// $host = $_SERVER['HTTP_HOST'];
-    
+  /**
+   * Provide functionality that show client info.
+   */
+  public function getClientInfo() {
+    $host = \Drupal::request()->getHost();
+    // $host = $_SERVER['HTTP_HOST'];
     $ip = \Drupal::request()->getClientIp();
     // $ip = $_SERVER['REMOTE_ADDR'];
-
     if (strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') !== FALSE) {
-    	$browser_name = "Chrome";
-  	} elseif (strpos($_SERVER['HTTP_USER_AGENT'], 'Firefox') !== FALSE) {
-  		$browser_name = "Firefox";
-  	} elseif (strpos($_SERVER['HTTP_USER_AGENT'], 'Opera') !== FALSE) {
-  		$browser_name = "Opera";
-  	} elseif (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE) {
+      $browser_name = "Chrome";
+    }
+    elseif (strpos($_SERVER['HTTP_USER_AGENT'], 'Firefox') !== FALSE) {
+      $browser_name = "Firefox";
+    }
+    elseif (strpos($_SERVER['HTTP_USER_AGENT'], 'Opera') !== FALSE) {
+      $browser_name = "Opera";
+    }
+    elseif (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE) {
       $browser_name = "Internet Explorer";
     }
-	
+
     $show_content = [
       '#theme' => 'client_info',
       '#ip' => $ip,
@@ -36,4 +39,5 @@ class Showinfo extends ControllerBase {
 
     return $show_content;
   }
+
 }
